1c92df6786d5fca245a14a87362d6ec13962fd00
MattPecht <mattjanpecht@gmail.com>
2024-12-16 17:33:28 +0100

    Added basic visualization
    Basic but functional visualization of the game, implemented a memorization period showing the correct tiles as green and incorrect as red. After 2 seconds all tiles become blue and when clicked on reveal their real color.

--- visualization.py
+++ visualization.py
@@ -1,19 +1,65 @@
 import pygame
+from board import Board
 
+# Variables setup
+board = Board(5, 4)  # Test board
+red = (255, 0, 0)
+green = (0, 255, 0)
+blue = (0, 0, 255)
+black = (0, 0, 0)
+window_height = 700
+window_width = 700
+tiles = []
+tile_size = 80
+memorize_time = 2000 #time given to memorize at the start (in milliseconds)
+
+# Pygame setup
 pygame.init()
+screen = pygame.display.set_mode((window_height, window_width))
+pygame.display.set_caption("Memory Game")
 
-screen = pygame.display.set_mode((800, 600))
+# Tile initialization
+for row in range(board.size):
+	for column in range(board.size):
+		rect = pygame.Rect(100 * (column + 1), 100 * (row + 1), tile_size, tile_size)
+		if board.board[row][column].is_filled:
+			color = green
+		else:
+			color = red
+		tiles.append({"rect": rect, "color": color, "is_filled": board.board[row][column].is_filled})
 
-player = pygame.Rect((300, 250, 50, 50))
+# Pre-Game (Shows tiles' real colors to remember them)
+start_time = pygame.time.get_ticks()
+screen.fill(black)
+for tile in tiles:
+	pygame.draw.rect(screen, tile["color"], tile["rect"])
+pygame.display.flip()
+for tile in tiles:
+	tile["color"] = blue
 
+# Main game loop
 run = True
-
 while run:
-	screen.fill((0, 0, 0))
-	
-	for event in pygame.event.get():
-		if event.type == pygame.QUIT:
-			run = False
-	pygame.display.update()
+	if pygame.time.get_ticks() - start_time > memorize_time:	
+		for tile in tiles:
+			pygame.draw.rect(screen, tile["color"], tile["rect"])
 
+		# Event handling
+		for event in pygame.event.get():
+			if event.type == pygame.QUIT:
+				run = False
+			if event.type == pygame.MOUSEBUTTONDOWN:
+				for tile in tiles:
+					if tile["rect"].collidepoint(event.pos):  # Check if the tile was clicked
+						if tile["is_filled"]:
+							tile["color"] = green  # Change color to green if correct
+						else:
+							tile["color"] = red # Change color to red if incorrect (should be replaced by losing lives later)
+	else:
+		for event in pygame.event.get():
+			if event.type == pygame.QUIT:
+				run = False
+			if event.type == pygame.MOUSEBUTTONDOWN:
+				pass #Blocking mouse input during memorization phase
+	pygame.display.flip()
 pygame.quit()
